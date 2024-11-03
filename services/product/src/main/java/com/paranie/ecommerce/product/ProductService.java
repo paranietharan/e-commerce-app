733cc4f28a421b8907ef37084601890b2d8998e5
paranietharan <paranietharan20@gmail.com>
2024-11-03 11:18:42 +0530

    product service

---
+++ services/product/src/main/java/com/paranie/ecommerce/product/ProductService.java
@@ -0,0 +1,9 @@
+package com.paranie.ecommerce.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+}
