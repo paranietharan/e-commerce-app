e5046e439f8c61354218cee25c58dae9b4605fd0
paranietharan <paranie20@gmail.com>
2024-11-22 21:20:54 +0530

    kafka added in pom.xml

---
+++ services/order/src/main/java/com/paranie/ecommerce/config/KafkaOrderTopicConfig.java
@@ -0,0 +1,11 @@
+package com.paranie.ecommerce.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KafkaOrderTopicConfig {
+
+    @Bean
+}
